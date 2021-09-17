a31746a20fb36a62d234b79bb72f7dcd4b650267
david <david.perov60@gmail.com>
2021-09-17 16:48:49 +0300

    работа с базой деталей

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from "./app.module";
 
 (async function start (){
     const PORT = process.env.PORT || 5000;
-    const app = await NestFactory.create(AppModule, { cors: true })
+    const app = await NestFactory.create(AppModule, { 
+        cors: true,
+        logger: ['verbose']
+    })
     app.setGlobalPrefix('api');
 
     {
