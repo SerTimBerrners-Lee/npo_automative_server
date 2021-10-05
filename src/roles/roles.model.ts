0eca6ce16199102a41242bcc60193b8b227b19cf
david <david.perov60@gmail.com>
2021-10-05 22:58:32 +0300

    Добавил автосоздание разрешений

--- src/roles/roles.model.ts
+++ src/roles/roles.model.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { Model, Column, DataType, Table, HasMany } from "sequelize-typescript";
+import { Model, Column, DataType, Table, HasMany, BeforeCreate, AfterCreate } from "sequelize-typescript";
 import { User } from "src/users/users.model";
 
 interface RoleCreationAttrs {
@@ -22,6 +22,95 @@ export class Role extends Model<Role, RoleCreationAttrs> {
     @Column({type: DataType.STRING, allowNull: false})
     description: string;
 
+    @ApiProperty({example: '{}', description: 'Разрешения'})
+    @Column({type: DataType.TEXT, allowNull: true})
+    assets: string;
+
     @HasMany(() => User)
     user: User[];
+
+    @AfterCreate
+    static async createRole(role: any) {
+        role.assets = JSON.stringify({
+            product: true,
+            workResult: true,
+            linrary: true,
+            issue: true,
+            baseCbed: true,
+            baseDetal: true,
+            baseMaterial: true,
+            baseTools: true,
+            baseEquipment: true,
+            baseTech: true,
+            baseProvider: true,
+            baseBuyer: true,
+            baseFile: true,
+            issueShipments: true,
+            sclad: true,
+            metalloworking: true,
+            assembly: true,
+            brak: true,
+            trash: true,
+            writeOff: true,
+            raport: true,
+            complaint:true,
+            archive: true,
+            spare: true,
+
+            productAssets: {
+                read: true,
+                writeYour: true,
+                writeSomeone: true
+            },
+            basefileAssets: {
+                read: true,
+                writeYour: true,
+                writeSomeone: true
+            },
+            userWorkingAssets: {
+                myIssue: true,
+                planOperation: true,
+                resultWork: true,
+                library: true,
+            },
+            costPriceAssets: {
+                read: true
+            },
+            usersListAssets: {
+                hideUserData: false,
+                read: true,
+                writeYour: true,
+                writeSomeone: true
+            },
+            techProcessAssets: {
+                zag: true,
+                tok: true,
+                sles: true,
+                termo: true,
+                hrezer: true
+            },
+            settingsAssets: {
+                read: true,
+                edit: true,
+                users: true,
+                edizm: true,
+                baseMat: true,
+                basePokDet: true,
+                baseRash: true,
+                operation: true,
+                baseTools: true,
+                baseEquipment: true,
+                baseTech: true,
+                deficit: true,
+                workTime: true,
+                rols: true,
+                dolzn: true,
+                library: true,
+                priceHors: true,
+                baseData: true
+            }
+        })
+
+        await role.save()
+    }
 }    
