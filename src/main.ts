d1e9d29486da3202a9e157f8659896577e4d4807
david <david.perov60@gmail.com>
2022-03-09 15:27:23 +0300

    cors edit

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from "./app.module";
 (async function start (){
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule, { 
-        cors: process.env.APPLICATION_TYPE == 'DEVELOPMENT' ? true : false,
+        cors: true,
         logger: ['verbose']
     })
     app.setGlobalPrefix('api'); 
