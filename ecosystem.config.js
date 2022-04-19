a1bbc7ad0781c0634120427423fe040c107048b7
david <david.perov60@gmail.com>
2022-04-19 22:44:58 +0300

    change ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,13 +4,20 @@ module.exports = {
         name: "server",
         script: "./dist/main.js",
         watch: true,
+        instances: 8,
+        exec_mode: 'cluster',
+        node_args: '--max_old_space_size=4096',
+        autorestart: true,
+        max_memory_restart: '4G',
+        exp_backoff_restart_delay: 100,
+        time: true,
         env: {
-            "PORT": 5000,
-            "NODE_ENV": "dev"
+          "PORT": 3000,
+          "NODE_ENV": "dev"
         },
         env_production: {
-            "PORT": 5000,
-            "NODE_ENV": "prod",
+          "PORT": 3000,
+          "NODE_ENV": "prod",
         }
       }
   ]
