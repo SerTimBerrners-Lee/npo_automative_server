717550ca0c20ab609a725f50212a28ee8d96b4cb
david <david.perov60@gmail.com>
2022-04-19 22:59:06 +0300

    edit make file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,17 +8,30 @@ module.exports = {
         exec_mode: 'cluster',
         node_args: '--max_old_space_size=4096',
         autorestart: true,
-        max_memory_restart: '4G',
+        max_memory_restart: '1G',
         exp_backoff_restart_delay: 100,
         time: true,
         env: {
-          "PORT": 3000,
+          "PORT": 5000,
           "NODE_ENV": "dev"
         },
         env_production: {
-          "PORT": 3000,
+          "PORT": 5000,
           "NODE_ENV": "prod",
         }
       }
-  ]
+  ],
+  deploy: {
+    prod: {
+      user: 'npo',
+      host: [{
+        host: '89.23.4.133',
+        port: '22',
+      }],
+      repo: 'https://github.com/SerTimBerrners-Lee/npo_automative_server',
+      ref: 'origin/master',
+      path: '/home/npo/Desktop/npo_automative_server',
+      'post-deploy': 'npm install && pm2 startOrReload ecosystem.config.js --env production'
+    }
+  }
 }
--- makefile
+++ makefile
@@ -5,7 +5,7 @@ git:
 build:
 	nest build
 start:
-	pm2 start dist/main.js --name server --node-args="--max-old-space-size=4096" -i max
+	pm2 start ecosystem.config.js --env production
 all:
 	sudo make git
 	sudo make build
